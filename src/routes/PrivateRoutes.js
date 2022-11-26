b17e457cb2f88a609c8f424045d47e23f115f2a9
Hashir Baig <hashirbaig456@gmail.com>
2022-11-26 15:43:01 +0500

    Fixed persistance bug

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -1,12 +1,46 @@
-import React from "react"
-import { Outlet, Navigate } from "react-router-dom"
+import React, { useEffect } from "react"
+import { Outlet, Navigate, useNavigate } from "react-router-dom"
 import { AllRoutesMap } from "./RoutesConfig"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { authenticate } from "../redux/features/auth/authSlice"
+import { getCurrentUser, setAuthToken } from "../services/api"
 
 function PrivateRoutes() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const isTokenAuthenticated = useSelector(state => state.auth.isTokenAuthenticated)
 
-  return isTokenAuthenticated ? <Outlet /> : <Navigate to={AllRoutesMap.signIn} />
+  const checkForToken = async () => {
+    //Check for token
+    try {
+      const token = localStorage.token
+      await getCurrentUser()
+      setAuthToken(token)
+      dispatch(authenticate({ isTokenAuthenticated: true }))
+      navigate(AllRoutesMap.home)
+    } catch (error) {
+      console.log(error)
+      // Redirect back to sign-in page
+      dispatch(authenticate({ isTokenAuthenticated: false }))
+      navigate(AllRoutesMap.signIn)
+    }
+  }
+
+  useEffect(() => {
+    checkForToken()
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return isTokenAuthenticated ? (
+    <Outlet />
+  ) : !isTokenAuthenticated && localStorage.token ? ( // Refactor: Not sure if token has expired
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <img className="h-20 w-20 border rounded-xl animate-pulse" src="/assets/images/yaad-logo.png" alt="Yaad Logo" />
+    </div>
+  ) : (
+    <Navigate to={AllRoutesMap.signIn} />
+  )
 }
 
 export default PrivateRoutes
