a6d842308b8727bf75f233b2b5379d4235d8f8e1
Hashir Baig <hashirbaig456@gmail.com>
2022-12-07 14:59:49 +0500

    Added aws as backend hosting server

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,9 +1,9 @@
 const dev = {
   API_URL: "http://localhost:5000/api",
 }
-// API_URL: "https://meri-yaad.herokuapp.com/api",
+
 const prod = {
-  API_URL: "https://meri-yaad-core.herokuapp.com/api",
+  API_URL: "http://ec2-54-64-48-118.ap-northeast-1.compute.amazonaws.com:80/api",
 }
 
 const config = process.env.NODE_ENV === "production" ? prod : dev
